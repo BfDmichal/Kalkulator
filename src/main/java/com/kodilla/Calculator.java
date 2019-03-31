ad0585cdf14f650095c34c92b6d2de3914dad46f
BfDmichal <michal.przyborowski@o2.pl>
2019-03-31 15:32:23 +0200

    Second commit

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -3,13 +3,14 @@ package com.kodilla;
 public class Calculator {
 
     public void addition(){
-        System.out.println("Making total");
+        System.out.println("Make total");
     }
 
     public void subtraction(){
-        System.out.println("Making difference");
+        System.out.println("Make difference");
     }
     public static void main(String args[]){
         Calculator cal  = new Calculator();
+        cal.addition();
     }
 }
