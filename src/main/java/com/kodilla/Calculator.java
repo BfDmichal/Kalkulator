70695d777676afc03ab2b573d188498ad3be78f9
BfDmichal <michal.przyborowski@o2.pl>
2019-03-31 11:24:45 +0200

    call

---
+++ src/main/java/com/kodilla/Calculator.java
@@ -0,0 +1,15 @@
+package com.kodilla;
+
+public class Calculator {
+
+    public void addition(){
+        System.out.println("Making total");
+    }
+
+    public void subtraction(){
+        System.out.println("Making difference");
+    }
+    public static void main(String args[]){
+        Calculator cal  = new Calculator();
+    }
+}
